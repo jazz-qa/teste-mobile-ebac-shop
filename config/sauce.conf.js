1b1278d22cd7b98af6bf841d5d5ee6a6fbbcc957
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2026-04-06 22:16:20 -0300

    device name

--- config/sauce.conf.js
+++ config/sauce.conf.js
@@ -24,7 +24,7 @@ export let sauceConf = {
         {
             platformName: 'iOS',
             'appium:app': 'storage:filename=LojaEBAC.ipa', // The filename of the mobile app
-            'appium:deviceName': 'iPhone XR',
+            'appium:deviceName': 'iPhone 15 Simulator',
             // 'appium:platformVersion': '17',
             'appium:automationName': 'XCUITest',
             'sauce:options': {
